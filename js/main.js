42d95e286d16637bc540c20b435cad7b851adf4c
eloimarinho <eloimarinho97@gmail.com>
2024-09-13 00:45:16 -0300

    Primeiras alterações na página, no layout, conforme solicitado (html, css e imagens), também implentação simples no JS.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var missao = document.querySelector('.missao');
+var visao = document.querySelector('.visao');
+var valores = document.querySelector('.valores');
+var missaoClick = [1];
+var vissaoClick = [1];
+var valoresClick = [1];
+
+
+function clicar(acao, acaoClick){
+
+acao.addEventListener("click", function(event){
+    event.preventDefault();
+    acaoClick.push(1);
+    if (acaoClick.length % 2 == 0){
+        acao.children[2].classList.remove("none"); 
+    }else{
+        acao.children[2].classList.add("none"); 
+    }
+})
+}
+
+clicar(missao, missaoClick)
+clicar(visao, vissaoClick)
+clicar(valores, valoresClick)
